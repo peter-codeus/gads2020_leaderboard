5e1362f1bb02009b747850ee562e774642bde259
Peter Tsegwi <peter.tsegwi@mboadigital.tech>
2020-09-08 07:02:48 +0200

    updated project submission button background color

--- app/src/main/java/com/codeus/gadsleaderboard/GadsApi.java
+++ app/src/main/java/com/codeus/gadsleaderboard/GadsApi.java
@@ -3,14 +3,27 @@ package com.codeus.gadsleaderboard;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface GadsApi {
     public static String BASE_URL = "https://gadsapi.herokuapp.com/api/";
+    public static String FORMS_BASE_URL = "https://docs.google.com/forms/d/e/";
 
     @GET("skilliq")
     Call<List<GadsModel>> getTopSkillIQ();
 
     @GET("hours")
     Call<List<GadsModel>> getTopLearners();
+
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    @FormUrlEncoded
+    Call<Void> submitProject(
+            @Field("entry.1824927963") String email,
+            @Field("entry.1877115667") String firstName,
+            @Field("entry.2006916086") String lastName,
+            @Field("entry.284483984") String projectLink
+    );
 }
--- app/src/main/java/com/codeus/gadsleaderboard/ProjectSubmissionActivity.java
+++ app/src/main/java/com/codeus/gadsleaderboard/ProjectSubmissionActivity.java
@@ -2,15 +2,42 @@ package com.codeus.gadsleaderboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ProjectSubmissionActivity extends AppCompatActivity {
+
+    EditText firstNameEditText, lastNameEditText, emailEditText, projectLinkEditText;
+    Dialog submissionDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_submission);
+
+        firstNameEditText = findViewById(R.id.firstname_edit_text);
+        lastNameEditText = findViewById(R.id.lastname_edit_text);
+        emailEditText = findViewById(R.id.email_address_edit_text);
+        projectLinkEditText = findViewById(R.id.github_link_edit_text);
     }
 
     public void navigateBack(View view)
@@ -18,8 +45,72 @@ public class ProjectSubmissionActivity extends AppCompatActivity {
         finish();
     }
 
-    public void submitProject(View view)
+    public void displayConfirmationDialog(View view)
     {
+        submissionDialog = new Dialog(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.confirmation_dialog_layout, null);
+        Button yesBtn = dialogView.findViewById(R.id.confirm_dialog_yes_btn);
+        ImageView closeBtn = dialogView.findViewById(R.id.close_confirm_dialog_btn);
+        closeBtn.setOnClickListener(v -> { submissionDialog.hide(); });
+        yesBtn.setOnClickListener(v -> {
+            submissionDialog.hide();
+            submitProjectRequest();
+        });
 
+        submissionDialog.setContentView(dialogView);
+        submissionDialog.setCanceledOnTouchOutside(true);
+        submissionDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        submissionDialog.show();
+    }
+
+    public void displaySubmissionStatusDialog(boolean isSubmissionSuccessful)
+    {
+        Dialog submissionDialog = new Dialog(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.successful_submission_dialog_layout, null);
+        submissionDialog.setContentView(dialogView);
+        if(!isSubmissionSuccessful)
+        {
+            ImageView dialogImage = dialogView.findViewById(R.id.dialogIcon);
+            TextView textView = dialogView.findViewById(R.id.dialogText);
+            textView.setText("Submission not Successful");
+            dialogImage.setImageResource(R.drawable.ic_warning_red);
+        }
+        submissionDialog.setCanceledOnTouchOutside(true);
+        submissionDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        submissionDialog.show();
+    }
+
+    public void submitProjectRequest()
+    {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(GadsApi.FORMS_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        GadsApi gadsApi = retrofit.create(GadsApi.class);
+
+        String email = this.emailEditText.getText().toString();
+        String firstName = this.firstNameEditText.getText().toString();
+        String lastName = this.lastNameEditText.getText().toString();
+        String githubLink = this.projectLinkEditText.getText().toString();
+
+        Call<Void> dataCall = gadsApi.submitProject(email, firstName, lastName, githubLink);
+
+        dataCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                submissionDialog.hide();
+                if(response.isSuccessful())
+                    displaySubmissionStatusDialog(true);
+                else
+                    displaySubmissionStatusDialog(false);
+                Log.i("SUBMISSION", new Gson().toJson(response.body()));
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                submissionDialog.hide();
+                displaySubmissionStatusDialog(false);
+                Log.i("SUBMISSION", new Gson().toJson(t));
+            }
+        });
     }
 }
